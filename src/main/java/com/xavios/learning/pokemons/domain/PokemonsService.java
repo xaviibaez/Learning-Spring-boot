1d983a91acc26d3172b96444dc59310f11320e8f
Xavi Ibáñez Martínez <241xavi@gmail.com>
2024-03-02 12:44:14 +0100

    Add pokemons assembler and iteration tests

--- src/main/java/com/xavios/learning/pokemons/domain/PokemonsService.java
+++ src/main/java/com/xavios/learning/pokemons/domain/PokemonsService.java
@@ -10,14 +10,18 @@ import java.util.Map;
 @Service
 public class PokemonsService {
     public Mono<Map<String, Object>> getPokemons() {
+        //TODO: deberia comprobar en el flujo principal si la respues del cliente es vacia?
         return getAllPokemons()
                 .map(this::assemblerResponse);
     }
 
     private Mono<List<Map<String, Object>>> getAllPokemons() {
-        return pokemonsWebClient.getAllPokemons(0).flatMap(x -> {
-            pokemonsWebClient.getAllPokemons(20);
-            return Mono.just(List.of(x));
+        return pokemonsWebClient.getPokemons(0).flatMap(x -> {
+            pokemonsWebClient.getPokemons(20);
+
+            List<Map<String, Object>> results = (List<Map<String, Object>>)(x.get("results"));
+
+            return Mono.just(results != null ? results : List.of(x));
         });
     }
 
--- src/test/java/com/xavios/learning/pokemons/domain/PokemonsServiceTest.java
+++ src/test/java/com/xavios/learning/pokemons/domain/PokemonsServiceTest.java
@@ -20,7 +20,7 @@ class PokemonsServiceTest {
 
     @Test
     void it_should_return_not_null() {
-        when(pokemonsWebClient.getAllPokemons(0)).thenReturn(Mono.just(Map.of()));
+        when(pokemonsWebClient.getPokemons(0)).thenReturn(Mono.just(Map.of()));
 
         assertThat(pokemonsService.getPokemons())
                 .as("It should return not null")
@@ -29,19 +29,19 @@ class PokemonsServiceTest {
 
     @Test
     void it_should_call_pokemons_web_client() {
-        when(pokemonsWebClient.getAllPokemons(0)).thenReturn(Mono.just(Map.of()));
+        when(pokemonsWebClient.getPokemons(0)).thenReturn(Mono.just(Map.of()));
 
         StepVerifier.create(pokemonsService.getPokemons())
                 .as("It should call pokemons web client")
                 .assertNext(x -> verify(pokemonsWebClient, atLeast(1)
                         .description("It should call pokemons web client"))
-                        .getAllPokemons(0))
+                        .getPokemons(0))
                 .verifyComplete();
     }
 
     @Test
     void it_should_return_expected_pokemons_list() {
-        when(pokemonsWebClient.getAllPokemons(0)).thenReturn(Mono.just(pokemonsClientResponse));
+        when(pokemonsWebClient.getPokemons(0)).thenReturn(Mono.just(pokemonsClientResponse));
 
         StepVerifier.create(pokemonsService.getPokemons())
                 .as("it should return expected pokemons list")
@@ -51,19 +51,34 @@ class PokemonsServiceTest {
 
     @Test
     void it_should_call_2_times_pokemons_client() {
-        /*verify(pokemonsWebClient,
-                times(2)
-                        .description("It should call two times pokemons client"))
-                .getAllPokemons(anyInt());*/
-
-        //TODO: Este tests es correcto?
-        when(pokemonsWebClient.getAllPokemons(anyInt())).thenReturn(Mono.just(Map.of()));
+        when(pokemonsWebClient.getPokemons(anyInt())).thenReturn(Mono.just(Map.of()));
 
         StepVerifier.create(pokemonsService.getPokemons())
                 .as("It should call pokemons web client")
                 .assertNext(x -> verify(pokemonsWebClient, times(2)
                         .description("It should call pokemons web client"))
-                        .getAllPokemons(anyInt()))
+                        .getPokemons(anyInt()))
+                .verifyComplete();
+    }
+
+    @Test
+    void it_should_return_expected_pokemons_list_assembled() {
+        when(pokemonsWebClient.getPokemons(anyInt())).thenReturn(Mono.just(POKEMONS_CLIENT_RESPONSE_PAGE_0));
+
+        StepVerifier.create(pokemonsService.getPokemons())
+                .as("it should return expected pokemons list assembled")
+                .expectNext(POKEMONS_SERVICE_ASSEMBLED_RESPONSE_PAGE_0)
+                .verifyComplete();
+    }
+
+    @Test
+    void it_should_return_expected_pokemons_list_paginated() {
+        when(pokemonsWebClient.getPokemons(0)).thenReturn(Mono.just(POKEMONS_CLIENT_RESPONSE_PAGE_0));
+        when(pokemonsWebClient.getPokemons(20)).thenReturn(Mono.just(POKEMONS_CLIENT_RESPONSE_PAGE_1));
+
+        StepVerifier.create(pokemonsService.getPokemons())
+                .as("it should return expected pokemons list paginated")
+                .expectNext(POKEMONS_CLIENT_RESPONSE_PAGE_1)
                 .verifyComplete();
     }
 
@@ -79,10 +94,36 @@ class PokemonsServiceTest {
     @Mock
     private Map<String, Object> pokemonsClientResponse;
 
-    private static final List<Map<String, Object>> POKEMONS_PAGE_1 = List.of(
-            Map.of("name", "bulbasaur", "url", "https://pokeapi.co/api/v2/pokemon/1/")
+    private final Map<String, Object> POKEMONS_CLIENT_RESPONSE_PAGE_0 = Map.of(
+            "count", 1302,
+            "next", "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
+            "previous", "",
+            "results", List.of(
+                    Map.of("name", "bulbasaur","url", "https://pokeapi.co/api/v2/pokemon/1/"),
+                    Map.of("name", "ivysaur", "url", "https://pokeapi.co/api/v2/pokemon/2/")
+            )
     );
-    private static final List<Map<String, Object>> POKEMONS_PAGE_2 = List.of(
-            Map.of("name", "bulbasaur2", "url", "https://pokeapi.co/api/v2/pokemon/2/")
+    private final Map<String, Object> POKEMONS_CLIENT_RESPONSE_PAGE_1 = Map.of(
+            "count", 1302,
+            "next", "https://pokeapi.co/api/v2/pokemon?offset=40&limit=20",
+            "previous", "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
+            "results", List.of(
+                    Map.of("name", "bulbasaur","url", "https://pokeapi.co/api/v2/pokemon/21/"),
+                    Map.of("name", "ivysaur", "url", "https://pokeapi.co/api/v2/pokemon/22/")
+            )
+    );
+    private final Map<String, Object> POKEMONS_SERVICE_ASSEMBLED_RESPONSE_PAGE_0 = Map.of(
+        "pokemons", List.of(
+                Map.of("name", "bulbasaur", "url", "https://pokeapi.co/api/v2/pokemon/1/"),
+                Map.of("name", "ivysaur", "url", "https://pokeapi.co/api/v2/pokemon/2/")
+        )
+    );
+    private final Map<String, Object> POKEMONS_SERVICE_ASSEMBLED_RESPONSE_PAGE_1 = Map.of(
+            "pokemons", List.of(
+                    Map.of("name", "bulbasaur", "url", "https://pokeapi.co/api/v2/pokemon/1/"),
+                    Map.of("name", "ivysaur", "url", "https://pokeapi.co/api/v2/pokemon/2/"),
+                    Map.of("name", "bulbasaur", "url", "https://pokeapi.co/api/v2/pokemon/21/"),
+                    Map.of("name", "ivysaur", "url", "https://pokeapi.co/api/v2/pokemon/22/")
+            )
     );
 }
